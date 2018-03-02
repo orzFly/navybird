32ba72d4ad84ca85a06cc8e74265e816115be889
Yeechan Lu <wz.bluesnow@gmail.com>
2018-03-02 08:46:18 +0800

    Remove fat arrows

--- index.js
+++ index.js
@@ -9,36 +9,53 @@ class NavybirdPromise extends Promise {
   }
 
   tap(fn) {
-    return this.then((val) => {
-      const ret = () => val;
-      return this.constructor.resolve(val).then(fn).then(ret);
-    })
+    const promiseConstructor = this.constructor;
+    return this.then(function tapHandle(val) {
+      return promiseConstructor.resolve(val)
+        .then(fn)
+        .then(function tapReturnValue() {
+          return val;
+        });
+    });
   }
 
   tapCatch(...args) {
-    return this.catch((err) => {
-      const ret = () => Promise.reject(err);
-      return Promise.resolve(err).then(catchFn(args)).then(ret);
+    const promiseConstructor = this.constructor;
+    return this.catch(function tapCatchHandle(err) {
+      return promiseConstructor.resolve(err)
+        .then(catchFn(args))
+        .then(function tapCatchValue() {
+          return promiseConstructor.reject(err);
+        });
     })
   }
 
   delay(ms) {
-    return this.tap(() => delay(ms));
+    return this.tap(function delayValue() {
+      return delay(ms)
+    });
   }
 
   thenReturn(value) {
-    return this.then(() => value)
+    return this.then(function thenReturnValue() {
+      return value
+    });
   }
 
   spread(fn) {
     if (typeof fn !== "function") {
       return apiRejection(FUNCTION_ERROR + classString(fn));
     }
-    return this.then((val) => fn(...val));
+    return this.then(function spreadValue(val) {
+      return fn(...val)
+    });
   }
 
   all() {
-    return this.then((val) => this.constructor.all(val));
+    const promiseConstructor = this.constructor;
+    return this.then(function allValue(val) {
+      return promiseConstructor.all(val)
+    });
   }
 }
 
@@ -60,7 +77,7 @@ const resolveWrapper = function (fn) {
   }
 }
 
-const catchFn = (args) => {
+const catchFn = function (args) {
   if (args.length > 2) {
     return pCatchIf(args.slice(0, args.length - 1), args[args.length - 1])
   } else if (args.length == 2) {
@@ -78,9 +95,9 @@ NavybirdPromise.map = require('./src/map')(NavybirdPromise, apiRejection, FUNCTI
 
 NavybirdPromise.defer = function () {
   let resolve, reject;
-  const promise = new NavybirdPromise(function () {
-    resolve = arguments[0];
-    reject = arguments[1];
+  const promise = new NavybirdPromise(function deferPromiseCapturer(_resolve, _reject) {
+    resolve = _resolve;
+    reject = _reject;
   });
   return {
     resolve: resolve,
