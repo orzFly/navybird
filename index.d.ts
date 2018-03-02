904211d5df7ade52f8d6193ab45f071acbf283c5
Yeechan Lu <wz.bluesnow@gmail.com>
2018-03-02 09:27:21 +0800

    Add TypeScript declaration

---
+++ index.d.ts
@@ -0,0 +1,419 @@
+/* Type definitions for Navybird.
+ * The code following this comment originates from:
+ *   https://github.com/DefinitelyTyped/DefinitelyTyped/blob/c531c6fa14b3f71051cfb6e099cd0e9b90fcd754/types/bluebird/index.d.ts
+ */
+
+// Type definitions for bluebird 3.5
+// Project: https://github.com/petkaantonov/bluebird
+// Definitions by: Leonard Hecker <https://github.com/lhecker>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 2.3
+/*!
+ * The code following this comment originates from:
+ *   https://github.com/types/npm-bluebird
+ *
+ * Note for browser users: use bluebird-global typings instead of this one
+ * if you want to use Bluebird via the global Promise symbol.
+ *
+ * Licensed under:
+ *   The MIT License (MIT)
+ *
+ *   Copyright (c) 2016 unional
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *   THE SOFTWARE.
+ */
+
+type CatchFilter<E> =
+  | (new (...args: any[]) => E)
+  | ((error: E) => boolean)
+  | (object & E);
+
+declare class Navybird<R> implements PromiseLike<R> {
+  /**
+   * Create a new promise. The passed in function will receive functions `resolve` and `reject` as its arguments which can be called to seal the fate of the created promise.
+   * If promise cancellation is enabled, passed in function will receive one more function argument `onCancel` that allows to register an optional cancellation callback.
+   */
+  constructor(
+    callback: (
+      resolve: (thenableOrResult?: R | PromiseLike<R>) => void,
+      reject: (error?: any) => void,
+      onCancel?: (callback: () => void) => void
+    ) => void
+  );
+
+  /**
+   * Promises/A+ `.then()`. Returns a new promise chained from this promise.
+   *
+   * The new promise will be rejected or resolved depending on the passed `fulfilledHandler`, `rejectedHandler` and the state of this promise.
+   */
+  // Based on PromiseLike.then, but returns a Navybird instance.
+  then<U>(
+    onFulfill?: (value: R) => U | PromiseLike<U>,
+    onReject?: (error: any) => U | PromiseLike<U>
+  ): Navybird<U>; // For simpler signature help.
+  then<TResult1 = R, TResult2 = never>(
+    onfulfilled?: ((value: R) => TResult1 | PromiseLike<TResult1>) | null,
+    onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | null
+  ): Navybird<TResult1 | TResult2>;
+
+  /**
+   * This is a catch-all exception handler, shortcut for calling `.then(null, handler)` on this promise.
+   *
+   * Any exception happening in a `.then`-chain will propagate to nearest `.catch` handler.
+   *
+   * Alias `.caught();` for compatibility with earlier ECMAScript version.
+   */
+  catch(onReject: (error: any) => R | PromiseLike<R>): Navybird<R>;
+  catch<U>(
+    onReject: ((error: any) => U | PromiseLike<U>) | undefined | null
+  ): Navybird<U | R>;
+
+  /**
+   * This extends `.catch` to work more like catch-clauses in languages like Java or C#.
+   *
+   * Instead of manually checking `instanceof` or `.name === "SomeError"`,
+   * you may specify a number of error constructors which are eligible for this catch handler.
+   * The catch handler that is first met that has eligible constructors specified, is the one that will be called.
+   *
+   * This method also supports predicate-based filters.
+   * If you pass a predicate function instead of an error constructor, the predicate will receive the error as an argument.
+   * The return result of the predicate will be used determine whether the error handler should be called.
+   *
+   * Alias `.caught();` for compatibility with earlier ECMAScript version.
+   */
+  catch<E1, E2, E3, E4, E5>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    filter5: CatchFilter<E5>,
+    onReject: (error: E1 | E2 | E3 | E4 | E5) => R | PromiseLike<R>
+  ): Navybird<R>;
+  catch<U, E1, E2, E3, E4, E5>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    filter5: CatchFilter<E5>,
+    onReject: (error: E1 | E2 | E3 | E4 | E5) => U | PromiseLike<U>
+  ): Navybird<U | R>;
+
+  catch<E1, E2, E3, E4>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    onReject: (error: E1 | E2 | E3 | E4) => R | PromiseLike<R>
+  ): Navybird<R>;
+
+  catch<U, E1, E2, E3, E4>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    onReject: (error: E1 | E2 | E3 | E4) => U | PromiseLike<U>
+  ): Navybird<U | R>;
+
+  catch<E1, E2, E3>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    onReject: (error: E1 | E2 | E3) => R | PromiseLike<R>
+  ): Navybird<R>;
+  catch<U, E1, E2, E3>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    onReject: (error: E1 | E2 | E3) => U | PromiseLike<U>
+  ): Navybird<U | R>;
+
+  catch<E1, E2>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    onReject: (error: E1 | E2) => R | PromiseLike<R>
+  ): Navybird<R>;
+  catch<U, E1, E2>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    onReject: (error: E1 | E2) => U | PromiseLike<U>
+  ): Navybird<U | R>;
+
+  catch<E1>(
+    filter1: CatchFilter<E1>,
+    onReject: (error: E1) => R | PromiseLike<R>
+  ): Navybird<R>;
+  catch<U, E1>(
+    filter1: CatchFilter<E1>,
+    onReject: (error: E1) => U | PromiseLike<U>
+  ): Navybird<U | R>;
+
+  /**
+   * This is a catch-all exception handler, shortcut for calling `.then(null, handler)` on this promise.
+   *
+   * Any exception happening in a `.then`-chain will propagate to nearest `.catch` handler.
+   *
+   * Alias `.caught();` for compatibility with earlier ECMAScript version.
+   */
+  caught: Navybird<R>["catch"];
+
+  /**
+   * Like `.finally()`, but not called for rejections.
+   */
+  tap<U>(onFulFill: (value: R) => PromiseLike<U> | U): Navybird<R>;
+
+  /**
+   * Like `.catch()` but rethrows the error
+   */
+  tapCatch<U>(onReject: (error?: any) => U | PromiseLike<U>): Navybird<R>;
+
+  tapCatch<U, E1, E2, E3, E4, E5>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    filter5: CatchFilter<E5>,
+    onReject: (error: E1 | E2 | E3 | E4 | E5) => U | PromiseLike<U>
+  ): Navybird<R>;
+  tapCatch<U, E1, E2, E3, E4>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    filter4: CatchFilter<E4>,
+    onReject: (error: E1 | E2 | E3 | E4) => U | PromiseLike<U>
+  ): Navybird<R>;
+  tapCatch<U, E1, E2, E3>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    filter3: CatchFilter<E3>,
+    onReject: (error: E1 | E2 | E3) => U | PromiseLike<U>
+  ): Navybird<R>;
+  tapCatch<U, E1, E2>(
+    filter1: CatchFilter<E1>,
+    filter2: CatchFilter<E2>,
+    onReject: (error: E1 | E2) => U | PromiseLike<U>
+  ): Navybird<R>;
+  tapCatch<U, E1>(
+    filter1: CatchFilter<E1>,
+    onReject: (error: E1) => U | PromiseLike<U>
+  ): Navybird<R>;
+
+  /**
+   * Same as calling `Promise.delay(ms, this)`.
+   */
+  delay(ms: number): Navybird<R>;
+
+  /**
+   * Convenience method for:
+   *
+   * <code>
+   * .then(function() {
+   *    return value;
+   * });
+   * </code>
+   *
+   * in the case where `value` doesn't change its value. That means `value` is bound at the time of calling `.return()`
+   *
+   * Alias `.thenReturn();` for compatibility with earlier ECMAScript version.
+   */
+  return(): Navybird<void>;
+  return<U>(value: U): Navybird<U>;
+  thenReturn(): Navybird<void>;
+  thenReturn<U>(value: U): Navybird<U>;
+
+  /**
+   * Like calling `.then`, but the fulfillment value or rejection reason is assumed to be an array, which is flattened to the formal parameters of the handlers.
+   */
+  spread<U, W>(
+    fulfilledHandler: (...values: W[]) => U | PromiseLike<U>
+  ): Navybird<U>;
+  spread<U>(
+    fulfilledHandler: (...args: any[]) => U | PromiseLike<U>
+  ): Navybird<U>;
+
+  /**
+   * Same as calling `Promise.all(thisPromise)`. With the exception that if this promise is bound to a value, the returned promise is bound to that value too.
+   */
+  // TODO type inference from array-resolving promise?
+  all<U>(): Navybird<U[]>;
+
+  /**
+   * Same as calling `Navybird.map(thisPromise, mapper)`. With the exception that if this promise is bound to a value, the returned promise is bound to that value too.
+   */
+  // TODO type inference from array-resolving promise?
+  map<Q, U>(
+    mapper: (item: Q, index: number, arrayLength: number) => U | PromiseLike<U>,
+    options?: Navybird.ConcurrencyOption
+  ): Navybird<U[]>;
+
+  /**
+   * Create a promise that is resolved with the given `value`. If `value` is a thenable or promise, the returned promise will assume its state.
+   */
+  static resolve(): Navybird<void>;
+  static resolve<R>(value: R | PromiseLike<R>): Navybird<R>;
+
+  /**
+   * Create a promise with undecided fate and return a `PromiseResolver` to control it. See resolution?: Promise(#promise-resolution).
+   */
+  static defer<R>(): Navybird.Resolver<R>;
+
+  /**
+   * Returns a promise that will be resolved with value (or undefined) after given ms milliseconds.
+   * If value is a promise, the delay will start counting down when it is fulfilled and the returned
+   *  promise will be fulfilled with the fulfillment value of the value promise.
+   */
+  static delay<R>(ms: number, value: R | PromiseLike<R>): Navybird<R>;
+  static delay(ms: number): Navybird<void>;
+
+  /**
+   * Given an array, or a promise of an array, which contains promises (or a mix of promises and values) return a promise that is fulfilled when all the items in the array are fulfilled.
+   * The promise's fulfillment value is an array with fulfillment values at respective positions to the original array.
+   * If any promise in the array rejects, the returned promise is rejected with the rejection reason.
+   */
+  // TODO enable more overloads
+  // array with promises of different types
+  static all<T1, T2, T3, T4, T5>(
+    values: [
+      PromiseLike<T1> | T1,
+      PromiseLike<T2> | T2,
+      PromiseLike<T3> | T3,
+      PromiseLike<T4> | T4,
+      PromiseLike<T5> | T5
+    ]
+  ): Navybird<[T1, T2, T3, T4, T5]>;
+  static all<T1, T2, T3, T4>(
+    values: [
+      PromiseLike<T1> | T1,
+      PromiseLike<T2> | T2,
+      PromiseLike<T3> | T3,
+      PromiseLike<T4> | T4
+    ]
+  ): Navybird<[T1, T2, T3, T4]>;
+  static all<T1, T2, T3>(
+    values: [PromiseLike<T1> | T1, PromiseLike<T2> | T2, PromiseLike<T3> | T3]
+  ): Navybird<[T1, T2, T3]>;
+  static all<T1, T2>(
+    values: [PromiseLike<T1> | T1, PromiseLike<T2> | T2]
+  ): Navybird<[T1, T2]>;
+  static all<T1>(values: [PromiseLike<T1> | T1]): Navybird<[T1]>;
+  // array with values
+  static all<R>(
+    values:
+      | PromiseLike<Iterable<PromiseLike<R> | R>>
+      | Iterable<PromiseLike<R> | R>
+  ): Navybird<R[]>;
+
+  /**
+   * Promise.join(
+   *   Promise<any>|any values...,
+   *   function handler
+   * ) -> Promise
+   * For coordinating multiple concurrent discrete promises.
+   *
+   * Note: In 1.x and 0.x Promise.join used to be a Promise.all that took the values in as arguments instead in an array.
+   * This behavior has been deprecated but is still supported partially - when the last argument is an immediate function value the new semantics will apply
+   */
+  static join<R, A1>(
+    arg1: A1 | PromiseLike<A1>,
+    handler: (arg1: A1) => R | PromiseLike<R>
+  ): Navybird<R>;
+  static join<R, A1, A2>(
+    arg1: A1 | PromiseLike<A1>,
+    arg2: A2 | PromiseLike<A2>,
+    handler: (arg1: A1, arg2: A2) => R | PromiseLike<R>
+  ): Navybird<R>;
+  static join<R, A1, A2, A3>(
+    arg1: A1 | PromiseLike<A1>,
+    arg2: A2 | PromiseLike<A2>,
+    arg3: A3 | PromiseLike<A3>,
+    handler: (arg1: A1, arg2: A2, arg3: A3) => R | PromiseLike<R>
+  ): Navybird<R>;
+  static join<R, A1, A2, A3, A4>(
+    arg1: A1 | PromiseLike<A1>,
+    arg2: A2 | PromiseLike<A2>,
+    arg3: A3 | PromiseLike<A3>,
+    arg4: A4 | PromiseLike<A4>,
+    handler: (arg1: A1, arg2: A2, arg3: A3, arg4: A4) => R | PromiseLike<R>
+  ): Navybird<R>;
+  static join<R, A1, A2, A3, A4, A5>(
+    arg1: A1 | PromiseLike<A1>,
+    arg2: A2 | PromiseLike<A2>,
+    arg3: A3 | PromiseLike<A3>,
+    arg4: A4 | PromiseLike<A4>,
+    arg5: A5 | PromiseLike<A5>,
+    handler: (
+      arg1: A1,
+      arg2: A2,
+      arg3: A3,
+      arg4: A4,
+      arg5: A5
+    ) => R | PromiseLike<R>
+  ): Navybird<R>;
+
+  // variadic array
+  /** @deprecated use .all instead */
+  static join<R>(...values: Array<R | PromiseLike<R>>): Navybird<R[]>;
+
+  /**
+   * Map an array, or a promise of an array, which contains a promises (or a mix of promises and values) with the given `mapper` function with the signature `(item, index, arrayLength)` where `item` is the resolved value of a respective promise in the input array.
+   * If any promise in the input array is rejected the returned promise is rejected as well.
+   *
+   * If the `mapper` function returns promises or thenables, the returned promise will wait for all the mapped results to be resolved as well.
+   *
+   * *The original array is not modified.*
+   */
+  static map<R, U>(
+    values:
+      | PromiseLike<Iterable<PromiseLike<R> | R>>
+      | Iterable<PromiseLike<R> | R>,
+    mapper: (item: R, index: number, arrayLength: number) => U | PromiseLike<U>,
+    options?: Navybird.ConcurrencyOption
+  ): Navybird<U[]>;
+}
+
+declare namespace Navybird {
+  interface ConcurrencyOption {
+    concurrency: number;
+  }
+
+  /** @deprecated Use PromiseLike<T> directly. */
+  type Thenable<T> = PromiseLike<T>;
+
+  type ResolvableProps<T> = object &
+    { [K in keyof T]: PromiseLike<T[K]> | T[K] };
+
+  interface Resolver<R> {
+    /**
+     * Returns a reference to the controlled promise that can be passed to clients.
+     */
+    promise: Navybird<R>;
+
+    /**
+     * Resolve the underlying promise with `value` as the resolution value. If `value` is a thenable or a promise, the underlying promise will assume its state.
+     */
+    resolve(value: R): void;
+    resolve(): void;
+
+    /**
+     * Reject the underlying promise with `reason` as the rejection reason.
+     */
+    reject(reason: any): void;
+  }
+}
+
+export = Navybird;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const delay = require('delay');
-const pCatchIf = require('p-catch-if');
+const delay = require("delay");
+const pCatchIf = require("p-catch-if");
 
-class NavybirdPromise extends Promise {
+class Navybird extends Promise {
   caught(...args) {
     return Promise.prototype.catch.call(this, catchFn(args));
   }
@@ -11,7 +11,8 @@ class NavybirdPromise extends Promise {
   tap(fn) {
     const promiseConstructor = this.constructor;
     return this.then(function tapHandle(val) {
-      return promiseConstructor.resolve(val)
+      return promiseConstructor
+        .resolve(val)
         .then(fn)
         .then(function tapReturnValue() {
           return val;
@@ -22,23 +23,24 @@ class NavybirdPromise extends Promise {
   tapCatch(...args) {
     const promiseConstructor = this.constructor;
     return this.catch(function tapCatchHandle(err) {
-      return promiseConstructor.resolve(err)
+      return promiseConstructor
+        .resolve(err)
         .then(catchFn(args))
         .then(function tapCatchValue() {
           return promiseConstructor.reject(err);
         });
-    })
+    });
   }
 
   delay(ms) {
     return this.tap(function delayValue() {
-      return delay(ms)
+      return delay(ms);
     });
   }
 
   thenReturn(value) {
     return this.then(function thenReturnValue() {
-      return value
+      return value;
     });
   }
 
@@ -47,70 +49,85 @@ class NavybirdPromise extends Promise {
       return apiRejection(FUNCTION_ERROR + classString(fn));
     }
     return this.then(function spreadValue(val) {
-      return fn(...val)
+      return fn(...val);
+    });
+  }
+
+  map(mapper, opts) {
+    const promiseConstructor = this.constructor;
+    return this.then(function allValue(val) {
+      return promiseConstructor.map(val, mapper, opts);
     });
   }
 
   all() {
     const promiseConstructor = this.constructor;
     return this.then(function allValue(val) {
-      return promiseConstructor.all(val)
+      return promiseConstructor.all(val);
     });
   }
 }
 
-module.exports = NavybirdPromise;
+module.exports = Navybird;
 
 const FUNCTION_ERROR = "expecting a function but got ";
 
-const apiRejection = function (msg) {
-  return NavybirdPromise.reject(new NavybirdPromise.TypeError(msg));
+const apiRejection = function(msg) {
+  return Navybird.reject(new Navybird.TypeError(msg));
 };
 
-const classString = function (obj) {
+const classString = function(obj) {
   return {}.toString.call(obj);
-}
+};
 
-const resolveWrapper = function (fn) {
-  return function () {
-    return NavybirdPromise.resolve(fn.apply(this, arguments))
-  }
-}
+const resolveWrapper = function(fn) {
+  return function() {
+    return Navybird.resolve(fn.apply(this, arguments));
+  };
+};
 
-const catchFn = function (args) {
+const catchFn = function(args) {
   if (args.length > 2) {
-    return pCatchIf(args.slice(0, args.length - 1), args[args.length - 1])
+    return pCatchIf(args.slice(0, args.length - 1), args[args.length - 1]);
   } else if (args.length == 2) {
     return pCatchIf(args[0], args[1]);
   }
   return args[0];
-}
+};
 
-NavybirdPromise.TypeError = TypeError;
-NavybirdPromise.prototype['catch'] = NavybirdPromise.prototype.caught;
-NavybirdPromise.prototype['return'] = NavybirdPromise.prototype.thenReturn;
-NavybirdPromise.delay = resolveWrapper(delay);
-NavybirdPromise.isPromise = require('p-is-promise');
-NavybirdPromise.map = require('./src/map')(NavybirdPromise, apiRejection, FUNCTION_ERROR, classString);
+Navybird.TypeError = TypeError;
+Navybird.prototype["catch"] = Navybird.prototype.caught;
+Navybird.prototype["return"] = Navybird.prototype.thenReturn;
+Navybird.delay = resolveWrapper(delay);
+Navybird.isPromise = require("p-is-promise");
+Navybird.map = require("./src/map")(
+  Navybird,
+  apiRejection,
+  FUNCTION_ERROR,
+  classString
+);
 
-NavybirdPromise.defer = function () {
+Navybird.defer = function() {
   let resolve, reject;
-  const promise = new NavybirdPromise(function deferPromiseCapturer(_resolve, _reject) {
+  const promise = new Navybird(function deferPromiseCapturer(
+    _resolve,
+    _reject
+  ) {
     resolve = _resolve;
     reject = _reject;
   });
   return {
     resolve: resolve,
     reject: reject,
-    promise: promise
+    promise: promise,
   };
-}
+};
 
-NavybirdPromise.join = function (...args) {
+Navybird.join = function(...args) {
   const last = args.length - 1;
-  if (last > 0 && typeof args[last] === 'function') {
+  if (last > 0 && typeof args[last] === "function") {
     const fn = args.pop();
-    return NavybirdPromise.all(args).spread(fn);
+    return Navybird.all(args).spread(fn);
   }
-  return NavybirdPromise.all(args);
-}
+  return Navybird.all(args);
+};
