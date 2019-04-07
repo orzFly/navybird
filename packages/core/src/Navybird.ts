77892f2d325c367f1b0aaa93c526334b8eb09369
Yeechan Lu <git@orzfly.com>
2019-04-08 04:18:22 +0800

    Add defer

--- packages/core/src/Navybird.ts
+++ packages/core/src/Navybird.ts
@@ -1,24 +1,36 @@
-import { map, ConcurrencyOption } from './functions/map';
+import { Defer, defer } from './functions/defer';
 import { delay } from './functions/delay';
 import { isPromise } from './functions/isPromise';
 import { isPromiseLike } from './functions/isPromiseLike';
+import { ConcurrencyOption, map } from './functions/map';
 
 export class Navybird<T> extends Promise<T> {
   static isPromise: typeof isPromise = isPromise
   static isPromiseLike: typeof isPromiseLike = isPromiseLike
 
-  static map: NavybirdMap = map as any
-  static delay: NavybirdDelay = delay as any
+  static map: NavybirdMapFunction = map as any
+  static delay: NavybirdDelayFunction = delay as any
+  static defer: NavybirdDeferFunction = defer as any
+}
+
+export class NavybirdDefer<T> extends Defer<T> {
+  public readonly promise!: Navybird<T>
 }
 
 /**
  * @$TypeExpand typeof map
  * @$$Eval (str) => str.replace(/GenericPromise/g, "Navybird")
  */
-type NavybirdMap = <R, U>(iterable: Iterable<R | PromiseLike<R>> | PromiseLike<Iterable<R | PromiseLike<R>>>, mapper: (item: R, index: number) => U | PromiseLike<U>, opts: ConcurrencyOption) => Navybird<U[]>
+type NavybirdMapFunction = <R, U>(iterable: Iterable<R | PromiseLike<R>> | PromiseLike<Iterable<R | PromiseLike<R>>>, mapper: (item: R, index: number) => U | PromiseLike<U>, opts: ConcurrencyOption) => Navybird<U[]>
 
 /**
  * @$TypeExpand typeof delay
  * @$$Eval (str) => str.replace(/GenericPromise/g, "Navybird")
  */
-type NavybirdDelay = { <R>(ms: number, value: R | PromiseLike<R>): Navybird<R>; (ms: number): Navybird<void>; }
+type NavybirdDelayFunction = { <R>(ms: number, value: R | PromiseLike<R>): Navybird<R>; (ms: number): Navybird<void>; }
+
+/**
+ * @$TypeExpand typeof defer
+ * @$$Eval (str) => str.replace(/Defer</g, "NavybirdDefer<")
+ */
+type NavybirdDeferFunction = <T = any>() => NavybirdDefer<T>
---
+++ packages/core/src/functions/defer.ts
@@ -0,0 +1,29 @@
+import { GenericPromise, getPromiseConstructor } from '../helpers/getPromiseConstructor';
+
+export class Defer<T> {
+  constructor(Promise?: PromiseConstructor) {
+    let _resolve: this['resolve']
+    let _reject: this['reject']
+
+    this.promise = new Promise(function deferCapturePromiseExecutor(resolve, reject) {
+      _resolve = resolve;
+      _reject = reject;
+    });
+
+    this.resolve = _resolve;
+    this.reject = _reject;
+  }
+
+  public readonly promise!: GenericPromise<T>
+  public readonly resolve!: (value?: T | PromiseLike<T>) => void
+  public readonly reject!: (reason?: any) => void
+
+  get fulfill() {
+    return this.resolve;
+  }
+}
+
+export function defer<T = any>(): Defer<T> {
+  const Promise = getPromiseConstructor(this);
+  return new Defer<T>(Promise);
+};
